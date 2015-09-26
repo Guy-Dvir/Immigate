0260b53184e14fbd5a73c040b73b219713d0d1ab
Guy-Dvir <mr.guydvir@gmail.com>
2015-09-26 12:19:39 +0300

    slimmed down the architecture

--- gulpfile.js
+++ gulpfile.js
@@ -2,80 +2,68 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
 var jade        = require('gulp-jade');
-
-
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+var prefix      = require('gulp-autoprefixer');
 
 /**
- * Build the Jekyll Site
+ *launch the Server
  */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
-
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['sass', 'jade'], function() {
     browserSync({
         server: {
-            baseDir: '_site'
+            baseDir: ''
         },
         notify: false
     })
 });
 
 /**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+*error check
+*/
+function errorlog(error){
+   console.error.bind(error);
+   this.emit('end')
+}
+
+/**
+ * Compile main.sass into main.css
  */
 gulp.task('sass', function () {
-    return gulp.src('assets/css/main.scss')
+    return gulp.src('assets/css/main.sass')
         .pipe(sass({
             includePaths: ['css'],
             onError: browserSync.notify
         }))
+        .on ('error', errorlog)
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
 });
 
-
-/*
-jade
+/**
+*jade - convert main jade files into html in the root folder
 */
-
 gulp.task('jade', function () {
-  return gulp.src('_jadefiles/*.jade')
-  .pipe(jade())
-  .pipe(gulp.dest('_includes'));
-})
+  return gulp.src('_jadefiles/index.jade')
+  .pipe(jade({
+     pretty: true
+   }))
+  .on ('error', errorlog)
+  .pipe(gulp.dest(''))
+  .pipe(browserSync.reload({stream:true}))
+});
 
 /**
  * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
+ * Watch html/jade files, reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['sass']);
-    gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
-    gulp.watch(['_jadefiles/*.jade'], ['jade']);
+    gulp.watch('assets/css/**.sass', ['sass']);
+    gulp.watch('_jadefiles/**', ['jade'])
 });
 
 /**
  * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
+ * launch BrowserSync & watch files.
  */
 gulp.task('default', ['browser-sync', 'watch']);
