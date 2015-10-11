815b81b35091a1314510a712f1cc634124096328
Guy-Dvir <mr.guydvir@gmail.com>
2015-10-11 20:54:07 +0300

    Fixed for mobile again
    -address bar fix
    -image change only after float-ins
    -girl sizing aligned with address bar issue

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   clickthroughGirl();
   heroParallax();
   adjustViewHero()
-  girlSizing()
+  girlSizing($(window).height());
   focusSelectBtn();
 });
 
@@ -17,19 +17,19 @@ $(window).scroll(function(){
 
 $(window).on('resize', function(){
   mobileStableSize(Initheight)
-  girlSizing()
   adjustViewHero()
 });
 
-window.setInterval(function(){
-  bundleImgChange()
+setInterval(function(){
+  bundleImgChange(floatBoolean)
+  console.log(floatBoolean);
 }, 4000);
 
 
 /**General***************************************/
 
 //no jumpings from adress bar resizing in mobile browsers
-var Initheight = $(window).height()
+var Initheight = $(window).height();
 
 function mobileStableSize(Initheight) {
   var newHeight = $(window).height()
@@ -37,20 +37,28 @@ function mobileStableSize(Initheight) {
   var $sections = $('.home-sections section')
   var $header = $('.home-sections header')
 
-  if (newHeight <= Initheight + 100 && newHeight > Initheight){
-    $header.css('height', Initheight);
+  if (newHeight <= Initheight + 100 && newHeight > Initheight ||
+      newHeight >= Initheight - 100 && newHeight < Initheight)
+  {
+
+    $header.css('height', Initheight)
+    girlSizing(Initheight);
+
   } else {
       if (newHeight < 1000 && newWidth > 640){
-        $sections.css('height', '90vh');
-        $header.css('height', '100vh');
+        $sections.css('height', '90vh')
+        $header.css('height', '100vh')
+        girlSizing($(window).height());
       } else if(newHeight < 1000 && newWidth <= 640){
-          $sections.css('height', '100%');
-          $header.css('height', '100vh');
-      } else{
-          $sections.css('height', '50vh');
-          $header.css('height', '100vh');
+          $sections.css('height', '100%')
+          $header.css('height', '100vh')
+          girlSizing($(window).height());
+      } else if(newHeight > 1000 && newWidth > 640){
+          $sections.css('height', '50vh')
+          $header.css('height', '100vh')
+          girlSizing($(window).height());
       };
-      var Initheight = newHeight
+      window.Initheight = newHeight;
   }
 
 }
@@ -73,11 +81,15 @@ function smoothScroll (duration) {
 
 
 //Scroll triggered object float-in
+var floatBoolean = false;
+
 function floatIn(img){
     var wScroll = $(window).scrollTop();
+
     if(img.offset().top - 250 - $(window).height()/2 < wScroll +  $(window).height()/2) {
-        img.css('transform', 'translateY(0)');
-      }
+      img.css('transform', 'translateY(0)')
+      window.floatBoolean = true;
+    }
 }
 
 
@@ -97,8 +109,8 @@ function adjustViewHero(){
 }
 
 //size girl according to port view
-function girlSizing(){
-  var wHeight = $(window).height();
+function girlSizing(wHeight){
+  //var wHeight = $(window).height();
   $('.para-girl').css('height',(wHeight * 0.90) + 'px');
   $('.para-girl').css('width',(wHeight * 1.5) + 'px');
 }
@@ -125,15 +137,18 @@ function heroParallax() {
 }
 
 /**Bundle-section********************************/
-function bundleImgChange() {
+function bundleImgChange(floatBoolean) {
   var $img = $('.bundle-img');
 
-  $img.fadeOut(300, function(){
-    $img.toggleClass('job')
-    $img.toggleClass('res')
-    $img.fadeIn(300);
-  });
-
+  if (floatBoolean === true){
+    setTimeout(function () {
+      $img.fadeOut(300, function(){
+        $img.toggleClass('job')
+        $img.toggleClass('res')
+        $img.fadeIn(300);
+      });
+    }, 2500);
+  }
 }
 
 
